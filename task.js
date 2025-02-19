60a1d8e7161993f8d36894ad20305ca012a166e7
LucioGiocondo98 <togaluc@hotmail.com>
2025-02-19 18:11:25 +0100

    Fixed

--- task.js
+++ task.js
@@ -1,29 +1,42 @@
-const form = document.getElementById('form-for-task')
+const form = document.getElementById('form-for-task');
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('INVIO', form);
+
+  const taskInput = document.getElementById('newtask');
+  const taskValue = taskInput.value;
+  console.log('Valore', taskValue);
+
+  const taskContainer = document.getElementById('for-new-task');
+
+
+  const allNewTask = document.createElement('div');
+  allNewTask.classList.add('task-to-delete');
+  allNewTask.innerHTML = `
+    <table>
+      <tbody>
+        <tr>
+          <td>${taskValue}</td>
+          <td><button class="buttonForDelete">ELIMINA TASK</button></td>
+        </tr>
+      </tbody>
+    </table>
+  `;
+
+  taskContainer.appendChild(allNewTask);
+
+  taskInput.value = '';
+  console.log('Nuovo Valore', taskInput.value);
+  const deleteButton = allNewTask.querySelector('.buttonForDelete');
+  deleteButton.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log('INVIO',form)
-
-const taskInput = document.getElementById('newtask')
- let taskValue = taskInput.value;
- console.log('Valore',taskValue)
- 
- const allNewTask = document.createElement('table')
- allNewTask.classList.add('task-to-delete')
- allNewTask.innerHTML = `
- <tbody>
- <tr>
- <td>${taskValue}</td>
- </tr>
- <button> ELIMINA</button>
- </tbody>
- `
- const taskContainer =document.getElementById('for-new-task')
- taskContainer.appendChild(allNewTask)
-
- taskInput.value = '';
- console.log('Nuovo Valore',taskInput)
-
- 
+    taskContainer.removeChild(allNewTask);
+    console.log('Task eliminata:', taskValue);
+  });
 });
+
+
+
+
 
 
